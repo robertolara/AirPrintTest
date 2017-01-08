91eb72a555e67fa08e7dbf8034e15a5f07072c5d
robertolara <roberto.laraesp@gmail.com>
2017-01-08 01:07:59 -0600

    Initial Commit

---
+++ AirPrintTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirPrintTest
+//
+//  Created by Roberto E Lara on 1/8/17.
+//  Copyright © 2017 Roberto E Lara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
